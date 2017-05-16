e2163e1ac9335ab870f57771e53763665d7c4f25
Sam Lindsay <sam.lindsay@digital.justice.gov.uk>
2017-05-16 09:55:22 +0100

    Second exercise using select() and arrange()

---
+++ select_ex.R
@@ -0,0 +1,17 @@
+library(dplyr)
+library(ggplot2)
+
+# Different ways to select the arr_delay and dep_delay columns
+select(flights, arr_delay, dep_delay)
+select(flights, arr_delay:dep_delay)
+select(flights, ends_with("delay"))
+select(flights, contains("delay"))
+
+# Order flights by departure date and time
+arrange(flights, date, hour, minute)
+
+# Which flights were most delayed?
+arrange(flights, desc(dep_delay))[, 10]
+
+# Which flights caught up the most time during flight?
+arrange(flights, desc(dep_delay - arr_delay))
